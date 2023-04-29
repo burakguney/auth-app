ad0ca095ba123ede0b7a1a74d16535e92478b7b3
Burak Guney <burakguney.computer@gmail.com>
2023-04-30 01:08:17 +0300

    authentication controllers

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,6 +1,6 @@
 import crypto from 'crypto';
 
-const SECRET = 'BURAK-REST-API';
+const SECRET = 'FOLLOW-THE-WHITE-RABBIT';
 
 export const random = () => crypto.randomBytes(128).toString('base64');
 export const authentication = (salt: string, password: string) => {
