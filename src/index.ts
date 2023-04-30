84cda0f3d3684a224c7b56e3c8d9a50f2417f2e2
Burak Guney <burakguney.computer@gmail.com>
2023-04-30 10:25:34 +0300

    authentication routing and logging in

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import cookieParser from 'cookie-parser';
 import compression from 'compression';
 import cors from "cors";
 import mongoose from 'mongoose';
+import router from './router';
 
 const app = express();
 
@@ -22,8 +23,10 @@ server.listen(8080, () => {
     console.log('Server running on http://localhost:8080/');
 });
 
-const MONGO_URL = 'mongodb://localhost:27017'
+const MONGO_URL = 'mongodb://127.0.0.1:27017'
 
 mongoose.Promise = Promise;
 mongoose.connect(MONGO_URL);
 mongoose.connection.on('error', (error: Error) => console.log(error));
+
+app.use('/', router());
